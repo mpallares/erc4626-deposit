304dfe0a88021ac2de5076a810a59c5a24c7f54a
mpallares <mpallareslara@gmail.com>
2025-09-30 16:29:18 +0700

    Format Solidity files with forge fmt

--- src/TestToken.sol
+++ src/TestToken.sol
@@ -8,11 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * Uses OpenZeppelin's audited implementation
  */
 contract TestToken is ERC20 {
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint256 initialSupply
-    ) ERC20(name, symbol) {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
     }
 
